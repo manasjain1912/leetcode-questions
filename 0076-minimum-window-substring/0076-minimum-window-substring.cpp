2743c2c5fb051a9d400df67d8a445c1225453732
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-06-15 23:56:41 +0530

    Time: 33 ms (43.63%), Space: 7.7 MB (94.40%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -3,36 +3,52 @@ class Solution
 public:
   string minWindow(string s, string t)
   {
-       unordered_map<char, int> mp;
-        int minlen=INT_MAX, start=0;
-        for(auto ch:t){
-            mp[ch]++;
+    unordered_map<char, int> mp;
+    int ans = INT_MAX;
+    int n = s.size();
+    for (auto it : t)
+    {
+      mp[it]++;
+    }
+    int count = mp.size();
+    int start = 0;
+    int i = 0, j = 0;
+    while (j < n)
+    {
+      if (mp.find(s[j]) != mp.end())
+      {
+        mp[s[j]]--;
+        if (mp[s[j]] == 0)
+        {
+          count--;
         }
-        int i=0,j=0,count=mp.size();
-        while(j<s.length()){
-            if(mp.find(s[j])!=mp.end()){
-                mp[s[j]]--;
-                if(mp[s[j]]==0)
-                    count--;
+      }
+      if (count == 0)
+      {
+        while (count == 0)
+        {
+          if (mp.find(s[i]) != mp.end())
+          {
+            mp[s[i]]++;
+            if (mp[s[i]] == 1)
+            {
+              count++;
+                if(j-i+1<ans){
+                ans=j-i+1;
+                start=i;
+                }
             }
-            if(count==0){
-                while(count==0){
-                    if(mp.find(s[i])!=mp.end()){
-                        mp[s[i]]++;
-                        if(mp[s[i]]==1) {
-                            count++;
-                            if(j-i+1<minlen){
-                                minlen=j-i+1;
-                                start=i;
-                            }
-                        }
-                    }
-                    i++;
-                }  
-            }
-            j++;
+          }
+            i++;
         }
-        if(minlen==INT_MAX) return "";
-        return s.substr(start, minlen);
+        
+      }
+    j++;
+  }
+  if (ans == INT_MAX)
+  {
+    return "";
+  }
+  return s.substr(start, ans);
 }
 };
