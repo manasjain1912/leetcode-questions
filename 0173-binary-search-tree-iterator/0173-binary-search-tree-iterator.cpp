407c9477c561f1acd4224b062ef58a9f910d49ed
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-08-13 03:05:27 +0530

    Time: 22 ms (91.90%), Space: 24 MB (37.49%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -11,7 +11,7 @@
  */
 class BSTIterator {
 
-    private:stack<TreeNode*>st;
+    stack<TreeNode*>st;
     public:
     BSTIterator(TreeNode* root) {
         pushall(root);
@@ -27,7 +27,7 @@ class BSTIterator {
     bool hasNext() {
         return !st.empty();
     }
-    private: 
+   
     void pushall(TreeNode*root)
     {
         for(;root!=NULL;st.push(root),root=root->left);
