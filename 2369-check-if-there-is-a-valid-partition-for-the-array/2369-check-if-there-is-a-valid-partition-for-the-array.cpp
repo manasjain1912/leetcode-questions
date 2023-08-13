43594689d68e0b74962228d3a5b36f1f0d655d4a
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-08-13 15:37:37 +0530

    Time: 127 ms (89.85%), Space: 93 MB (15.08%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-bool dfs(vector<int> &nums,int n,int i,vector<int>& dp){
-    if(i>=n)return true;
-    bool ans1=false,ans2=false;
-    if(dp[i]!=-1)return dp[i];
-    if(i+2<n){
-        if(nums[i]==nums[i+1] && nums[i]==nums[i+2]){
-            ans1= dfs(nums,n,i+3,dp);
+    bool solve(int i,vector<int>&nums,vector<int>&dp)
+    {
+        if(i==nums.size())
+        {
+            return true;
         }
-        else if(nums[i]+1==nums[i+1] && nums[i+1]+1==nums[i+2]){
-            ans1= dfs(nums,n,i+3,dp);
+    if(dp[i]!=-1)
+    {
+        return dp[i];
+    }
+        if(i+1<nums.size() && nums[i+1]==nums[i])
+        {
+            if(solve(i+2,nums,dp)) return dp[i]=true;
+            if(i+2<nums.size() && nums[i+1]==nums[i+2])
+            {
+                if(solve(i+3,nums,dp)) return dp[i]=true;
+            }
         }
+        if(i+2<nums.size() && nums[i]+1==nums[i+1] && nums[i]+2==nums[i+2])
+        {
+            if(solve(i+3,nums,dp)) return dp[i]=true;
+        }
+        return dp[i]=false;
     }
-    if(i+1<n){
-        if(nums[i]==nums[i+1]){
-            ans2= dfs(nums,n,i+2,dp);
-        }   
+    bool validPartition(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return solve(0,nums,dp);
     }
-    return dp[i]=ans1||ans2;
-}
-bool validPartition(vector<int>& nums) {
-    int idx=0,n=nums.size();
-    vector<int> dp(n+1,-1);
-    return dfs(nums,n,idx,dp);
-}
 };
