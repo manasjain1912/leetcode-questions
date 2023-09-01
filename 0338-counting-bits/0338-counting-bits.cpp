9b84c255769eb5998b666cee5f98547d05e6113a
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-09-01 12:02:29 +0530

    Time: 6 ms (56.68%), Space: 7.7 MB (89.73%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> res(n + 1);
+        
+        for(int i = 1; i < n + 1; i++) {
+            if(i % 2 == 0) {
+                res[i] = res[i >> 1];                
+            }else{
+                res[i] = res[i >> 1] + 1;
+            }
+        }
+        
+        return res; 
+    }
+};
