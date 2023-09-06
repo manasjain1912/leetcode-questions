1fab17da66de4c63d6fd0f3d7d44f18a68b6c820
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-09-06 21:22:26 +0530

    Time: 145 ms (62.63%), Space: 29.9 MB (42.49%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& nums) 
-    {
-        vector<vector<int>>res;
-        sort(nums.begin(),nums.end());
+    vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
         for(int i=0;i<n-2;i++)
         {
             if(i==0 || (i>0 && nums[i]!=nums[i-1]))
             {
-                int lo=i+1,hi=n-1,sum=0-nums[i];
-                while(lo<hi)
+                int low=i+1,high=n-1,sum=-nums[i];
+                while(low<high)
                 {
-                    if(nums[lo]+nums[hi]==sum)
+                    if(nums[low]+nums[high]==sum)
                     {
-                         vector<int>temp;
+                        vector<int>temp;
                         temp.push_back(nums[i]);
-                        temp.push_back(nums[lo]);
-                        temp.push_back(nums[hi]);
-                        res.push_back(temp);
-                        while(lo<hi && nums[lo]==nums[lo+1])lo++;
-                        while(lo<hi && nums[hi]==nums[hi-1])hi--;
-                        lo++;hi--;
+                        temp.push_back(nums[low]);
+                        temp.push_back(nums[high]);
+                        ans.push_back(temp);
+                        while(low<high && nums[low]==nums[low+1])low++;
+                        while(low<high && nums[high]==nums[high-1])high--;
+                        low++;
+                        high--;
                     }
-                    else if(nums[lo]+nums[hi]<sum)
-                           lo++;
-                    else
-                        hi--;
+                    else if(nums[low]+nums[high]<sum)low++;   
+                    else high--;
                 }
             }
-            
         }
-        return res;
+        return ans;
     }
 };
