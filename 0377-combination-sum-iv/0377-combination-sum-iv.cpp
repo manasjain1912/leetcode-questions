046df9f88bcbd8efe817a4f6105df52347707566
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-09-09 15:28:23 +0530

    Time: 3 ms (50.37%), Space: 6.6 MB (85.45%) - LeetHub

---
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> sumArray(target+1, -1);
+        sumArray[0]=1;
+        countSum(nums, target, sumArray);
+        return sumArray[target];
+    }
+    int countSum(vector<int>& nums, int target, vector<int>& sumArray) {
+        if(sumArray[target]>-1)
+            return sumArray[target];
+        int sum=0;
+        for(int i=0 ; i<nums.size(); i++) {
+            if(nums[i] <= target)
+                sum += countSum(nums, target-nums[i], sumArray);
+                
+        }
+        sumArray[target] = sum;
+        return sum;
+    }
+};
