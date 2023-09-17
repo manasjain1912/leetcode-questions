280c757b99b99cf22ff5fd98b8908c72054d7e7f
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-09-17 16:55:31 +0530

    Time: 116 ms (24.80%), Space: 17.9 MB (45.64%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph){
+        int n=graph.size();
+        if(n==0||n==1)
+        return 0;
+        int endMask=(1<<n)-1;
+        queue<pair<int,int>> q;
+        set<pair<int,int>> st;
+        for(int i=0;i<n;i++)
+        {
+            q.push({i,1<<i});
+            st.insert({i,1<<i});
+        }
+        int level=0;
+        while(!q.empty())
+        {
+            int sz=q.size();
+            while(sz--)
+            {
+                pair<int,int> ele=q.front();
+                q.pop();
+                for(auto &x:graph[ele.first])
+                {
+                    int mask=ele.second|(1<<x);
+                    if(mask==endMask)
+                    return level+1;
+                    if(st.find({x,mask})==st.end())
+                    {
+                        q.push({x,mask});
+                        st.insert({x,mask});
+                    }
+                }
+            }
+            level++;
+        }
+         return 1;
+    }
+};
