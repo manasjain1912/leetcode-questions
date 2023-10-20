b929a6736518e7305829f3421e77e8b0cf7fd0a1
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-10-21 00:58:47 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (43.39%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums)
+    {
+        // optimized approach 
+        
+        
+        int n  = nums.size();
+        
+        int low=0 , high = n-1 , mid = 0;
+        
+        
+        while(mid<=high)
+        {
+            
+            
+            if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            
+            else if(nums[mid]==1)
+            {
+               mid++;
+            }
+            
+            
+            else
+            {
+                swap(nums[mid] , nums[high]);
+                high--;
+            }
+            
+            
+            
+            
+            
+            
+        }        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
