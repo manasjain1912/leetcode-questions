37f99b8ec54a30cd5a651e14f0e1b391cbff9f73
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-12-20 15:02:45 +0530

    Time: 202 ms (73.24%), Space: 135.3 MB (40.31%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-      vector<int>ans;
-        int n=arr.size();
-    deque<int>q;
-    int i=0,j=0;
-    while(j<n)
-    {
-        while(!q.empty() && arr[j]>q.back())
-        {
-            q.pop_back();
-        }
-        q.push_back(arr[j]);
-        if(j-i+1<k)
-        {
-            j++;
-        }
-        else if(j-i+1==k)
-        {
-            ans.push_back(q.front());
-            if(arr[i]==q.front())
-            {
-                q.pop_front();
-            }
-            i++;
-            j++;
-        }
-    }
-    return ans;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+     deque < int > dq;
+  vector < int > ans;
+  for (int i = 0; i < nums.size(); i++) {
+    if (!dq.empty() && dq.front() == i - k) dq.pop_front();
+
+    while (!dq.empty() && nums[dq.back()] < nums[i])
+      dq.pop_back();
+
+    dq.push_back(i);
+    if (i >= k - 1) ans.push_back(nums[dq.front()]);
+  }
+  return ans;
     }
 };
