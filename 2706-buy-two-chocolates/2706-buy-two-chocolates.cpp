f86bfbd78a6bf068e8d5cc6634addd6e3cee376b
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2023-12-20 14:43:37 +0530

    Time: 7 ms (91.14%), Space: 46.9 MB (94.77%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+         sort(prices.begin(), prices.end());
+        if(money-prices[0]-prices[1]<0) return money;
+        return money-prices[0]-prices[1];
+    }
+};
