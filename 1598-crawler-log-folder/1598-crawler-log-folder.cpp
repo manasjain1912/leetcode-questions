c52830ad9527214229042a84388950d49b4d9be2
Manas Jain <77676910+manasjain1912@users.noreply.github.com>
2024-07-10 11:38:26 +0530

    Time: 0 ms (100.00%), Space: 13.9 MB (84.88%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth=0;
+        for(const string& log : logs){
+            if(log == "../"){
+                if(depth>0){
+                    depth--;
+                }
+            }else if(log != "./"){
+                depth++;
+            }
+        }
+        return depth;
+    }
+};
